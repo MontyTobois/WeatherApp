d72489671493e09b012c091fbcfd5d4840b72517
Andre-Tobois <tobois.andre@gmail.com>
2019-01-04 23:47:38 -0500

    This is the first commit.

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Titles from  './components/Titles';
+import Form from  './components/Form';
+import Weather from  './components/Weather';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      Hello!!!
+      <Titles  />
+      <Form  />
+      <Weather />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Titles.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Titles extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Weather App</h1>
+        <p>Find the Temperature, conditions and more...</p>
+      </div>
+    )
+  }
+}
+
+
+export default Titles;
