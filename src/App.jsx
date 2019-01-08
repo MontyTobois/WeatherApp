52635e381f02a4119bf51ae2fe1bff974f19f5f8
Andre-Tobois <tobois.andre@gmail.com>
2019-01-07 23:22:56 -0500

    feat: Setup the state for weather.

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,20 @@ import Weather from  './components/Weather';
 import './App.css';
 
 //  Added You Own Api Key.
-const API_KEY = "";
+const API_KEY = "2c76e45c74caa212c1f0ebfdf66025d0";
 
 class App extends Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humdity: undefined,
+    description: undefined,
+    error: undefined
+  }
 
   getWeather = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
   const api_fetch = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}
